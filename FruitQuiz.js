843c4b60b7ea3a3a317278629c0c197262185a63
kacctus <kath1ynnho2908@gmail.com>
2025-01-12 16:11:45 +0800

    need to change the "back to home" and "share"

--- FruitQuiz.js
+++ FruitQuiz.js
@@ -4,96 +4,96 @@ function displayQuiz() {
             question: "You're thinking about volunteering for the first time. What excites you most?",
             choices: ["Helping people in need and making a real difference.", "Trying something new and meeting new people."],
             weights: [
-                {judgingScore: +3, perceivingScore: 0 }, // Weight for first choice
-                {perceivingScore: +1, judgingScore:0 }, // Weight for second choice
+                {social: +1, yapper: 0 }, // Weight for first choice
+                {social: +0, yapper:1 }, // Weight for second choice
             ]
         },
         {
             question: "You're looking at different volunteer opportunities on Google. What appeals to you most?",
             choices: ["A one-time event where you can make an immediate impact.", "A long-term commitment where you can build relationships."],
             weights: [
-                {thinkingScore: +1, feelingScore: 0 }, // Weight for first choice
-                {feelingScore: +3, thinkingScore: 0 } // Weight for second choice
+                {fixer: +1, longterm: 0 }, // Weight for first choice
+                {ixer: +0, longterm: 1  } // Weight for second choice
             ]
         },
         {
             question: "There's so many different opportunities available! But hmm... what motivates you most when considering a volunteer opportunity?",
             choices: ["The opportunity to make a tangible difference in the community.", "The chance to gain new experiences and meet new people."],
             weights: [
-                {intuitionScore: +3, sensingScore: 0 } , // Weight for first choice
-                {sensingScore: +1, intuitionScore: 0 } , // Weight for second choice
+                {tutor: +1, yapper: 0 } , // Weight for first choice
+                {tutor: +0, yapper: 1 } , // Weight for second choice
             ]
         },
         {
             question: "You realize that you can support a cause not only with your time, but also with your financial resources. Would you rather:",
             choices: ["Donate to a cause you believe in.", "Volunteer your time directly."],
             weights: [
-                {perceivingScore: +1, judgingScore: 0 } , // Weight for first choice
-                {judgingScore: +3, perceivingScore: 0 }, // Weight for second choice
+                {fixer: +1, longterm: 0} , // Weight for first choice
+                {fixer: +0, longterm: 1 }, // Weight for second choice
             ]
         },
         {
             question: "You asked a friend of yours to join you in your volunteering journey! He / she suggested to go for a sharing outreach event",
             choices: ["Sure! That sounds like fun!", "Nah.. I'm not comfortable around big groups of people yet!"],
             weights: [
-                {sensingScore: +1, intuitionScore: 0 } , // Weight for first choice
-                {intuitionScore: +3, sensingScore: 0 } , // Weight for second choice
+                {yapper: +1, fixer: 0} , // Weight for first choice
+                {yapper: +0, fixer: 1} , // Weight for second choice
             ]
         },
         {
             question:"You find something you're interested in! But it's a volunteering opportunity requires a lot of time and effort. Do you:",
             choices: ["Feel challenged and excited to take it on.", "Feel overwhelmed and look for something less demanding."],
             weights: [
-                {thinkingScore: +1, feelingScore: 0 } , // Weight for first choice
-                {feelingScore: +3, thinkingScore: 0 } , // Weight for second choice
+                {longterm: +1, fixer: 0 } , // Weight for first choice
+                {longterm: +0, fixer: 1 } , // Weight for second choice
             ]
         },
         {
             question:"Ding! A notification popped up! A neighbor asks you to tutor their child. Do you:",
             choices: ["Offer to help, even if it means adjusting your schedule", " Explain that you're not confident in your tutoring someone"],
             weights: [
-                {sensingScore: +1, intuitionScore: 0 } , // Weight for first choice
-                {intuitionScore: +3, sensingScore: 0 } , // Weight for intuition 
+                {tutor: +2, social:0} , // Weight for first choice
+                {tutor: +0, social:0 } , // Weight for intuition 
             ]
         },
         {
             question:"Well, that was a lot of research done for the day!",
             choices: ["Time to relax!", "Hopefully I can find something I'm passionate about soon...t"],
             weights: [
-                {introvertScore: +1, extrovertScore: 0 } , // Weight for first choice
-                {extrovertScore: +3, introvertScore: 0 } , // Weight for second choice
+                {tutor: +0, social:0 } , // Weight for first choice
+                {tutor: +0, social:0} , // Weight for second choice
             ]
         },
         {
             question:"You hop onto social media, and you come across a social media post about a local charity event. Do you:",
             choices: ["Share the post with your network and encourage others to attend or donate", "Simply like the post and move on"],
             weights: [
-                {thinkingScore: +3, feelingScore: 0 } , // Weight for first choice
-                {feelingScore: +1, thinkingScore: 0 } , // Weight for second choice
+                {social: +1, yapper: 1 } , // Weight for first choice
+                {social: +0, yapper: 0} , // Weight for second choice
             ]
         },
         {
             question:"As you scroll on social media, you also learn about an important social issue. Do you:",
             choices: ["Research the issue further and share information with your friends and family.", "Acknowledge the issue but not take any further action."],
             weights: [
-                {thinkingScore: +3, feelingScore: 0 } , // Weight for first choice
-                {feelingScore: +1, thinkingScore: 0 } , // Weight for second choice
+                {social: +1, yapper: 1} , // Weight for first choice
+                {social: +0, yapper: 0} , // Weight for second choice
             ]
         },
         {
             question:"Still not knowing what to do, you asked your parents for advice. Who's advice resonates with you more?",
             choices: ["Mum: Start small and find something you're passionate about.", "Dad: Use your strengths to find a volunteer role where you can truly make a difference."],
             weights: [
-                {extrovertScore: +3, introvertScore: 0 } , // Weight for first choice
-                {introvertScore: +1, extrovertScore: 0 } , // Weight for second choice
+                {longterm: +1, fixer: 1 } , // Weight for first choice
+                {social: +1, tutor: 1, yapper: 1} , // Weight for second choice
             ]
         },
         {
             question:"As you was falling asleep, you wondered: If I had a superpower to help me volunteer, what would it be?",
             choices: ["The ability to effortlessly connect with people from all walks of life.", "The ability to multiply yourself to volunteer at multiple places simultaneously."],
             weights: [
-                {perceivingScore: +3, judgingScore: 0 } , // Weight for first choice
-                {judgingScore: +1, perceivingScore: 0 } , // Weight for second choice
+                {yapper: +1, longterm: 0 } , // Weight for first choice
+                {tutor: +1, longterm:1 } , // Weight for second choice
             ]
         },
         {
@@ -117,7 +117,11 @@ function displayQuiz() {
     let intuitionScore = 0;
     let thinkingScore = 0;
     let feelingScore = 0;
-
+    let longterm = 0;
+    let fixer = 0;
+    let social = 0;
+    let tutor = 0;
+    let yapper = 0;
 
     function displayQuestionImage(questionIndex) {
         const imageURLs = [
@@ -133,7 +137,7 @@ function displayQuiz() {
             "qn 10.png",
             "qn 11.png",
             "qn 12.png",
-            "processing.GIF",
+            "loading.gif",
         ];
         const questionImageElement = document.getElementById('question-image');
         questionImageElement.src = imageURLs[questionIndex];
@@ -195,29 +199,20 @@ function displayQuiz() {
         console.log("Selected choice weight:", selectedChoiceWeight);
 
                 //Update scores based on weight of selected choice
-                if (selectedChoiceWeight.hasOwnProperty('introvertScore')) {
-                    introvertScore += selectedChoiceWeight.introvertScore;
+                if (selectedChoiceWeight.hasOwnProperty('longterm')) {
+                    longterm += selectedChoiceWeight.longterm;
                 }
-                if (selectedChoiceWeight.hasOwnProperty('extrovertScore')) {
-                    extrovertScore += selectedChoiceWeight.extrovertScore;
+                if (selectedChoiceWeight.hasOwnProperty('fixer')) {
+                    fixer += selectedChoiceWeight.fixer;
                 }
-                if (selectedChoiceWeight.hasOwnProperty('judgingScore')) {
-                    judgingScore += selectedChoiceWeight.judgingScore;
+                if (selectedChoiceWeight.hasOwnProperty('social')) {
+                    social += selectedChoiceWeight.social;
                 }
-                if (selectedChoiceWeight.hasOwnProperty('perceivingScore')) {
-                    perceivingScore += selectedChoiceWeight.perceivingScore;
+                if (selectedChoiceWeight.hasOwnProperty('tutor')) {
+                    tutor += selectedChoiceWeight.tutor;
                 }
-                if (selectedChoiceWeight.hasOwnProperty('sensingScore')) {
-                    sensingScore += selectedChoiceWeight.sensingScore;
-                }
-                if (selectedChoiceWeight.hasOwnProperty('intuitionScore')) {
-                    intuitionScore += selectedChoiceWeight.intuitionScore;
-                }
-                if (selectedChoiceWeight.hasOwnProperty('thinkingScore')) {
-                    thinkingScore += selectedChoiceWeight.thinkingScore;
-                }
-                if (selectedChoiceWeight.hasOwnProperty('feelingScore')) {
-                    feelingScore += selectedChoiceWeight.feelingScore;
+                if (selectedChoiceWeight.hasOwnProperty('yapper')) {
+                    yapper += selectedChoiceWeight.yapper;
                 }
 
             //Move to the next question
@@ -234,15 +229,13 @@ function displayQuiz() {
     //Function to calculate MBTI type based on scores and display image
     function calculateMBTITypeAndDisplayImage() {
         //Calculate introvert/extrovert dimension
-        const introextro = introvertScore > extrovertScore ? "I" :"E";
+        const longfix = longterm > fixer ? "L" :"F";
         //Calculate sensing/intuition dimension
-        const sensint = sensingScore > intuitionScore ? "S" : "N";
+        const socialtutor = social > tutor ? "S" : "T";
         //Calculate thinking/feeling dimension
-        const thinkfeel = thinkingScore > feelingScore ? "T" : "F";
-        //Calculate judging/perceiving dimension
-        const judgeper = judgingScore > perceivingScore ? "J" : "P";
+        const tutoryapper = tutor > yapper ? "T" : "Y";
         //Produce MBTI type string
-        const mbtiTypeString = introextro + sensint + thinkfeel + judgeper;
+        const mbtiTypeString = longfix+socialtutor+tutoryapper
 
         console.log("MBTI Type:", mbtiTypeString);
 
@@ -266,22 +259,14 @@ function displayQuiz() {
     //Function to calculate MBTI type and return image URL 
         function getMBTIImageUrl(mbtiTypeString) {
             const MBTIImageUrls = {
-                "ENTJ": "new long term.png",
-                "INTJ": "new quick fixer.png", 
-                "ENTP": "new social savvy.png",
-                "INTP": "new tutor.png",
-                "ENFJ": "new yapper.png",
-                "INFJ": "new long term.png",
-                "ENFP": "new quick fixer.png",
-                "INFP": "new social savvy.png",
-                "ESFJ": "new tutor.png",
-                "ISFJ": "new yapper.png",
-                "ESTJ": "new long term.png",
-                "ISTJ": "new quick fixer.png",
-                "ESTP": "new social savvy.png",
-                "ISTP": "new tutor.png",
-                "ESFP": "new yapper.png",
-                "ISFP": "new yapper.png",
+                "LST": "new long term.png",
+                "LSY": "new yapper.png",
+                "LTT": "new tutor.png",
+                "LTY": "new long term.png",
+                "FST": "new fixer.png",
+                "FSY": "new social savvy.png",
+                "FTT": "new tutor.png",
+                "FTY": "new social savvy.png"
             };
             return MBTIImageUrls[mbtiTypeString] || ""
         }
